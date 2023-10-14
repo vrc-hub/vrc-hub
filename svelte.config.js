cecf78b41faad377f70058d3d9e171444047ea59
Luna Simons <luna@bddvlpr.com>
2023-10-14 22:06:23 +0200

    feat: initialized tailwindcss and preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
+import { preprocessMeltUI } from '@melt-ui/pp';
+import sequence from 'svelte-sequential-preprocessor';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
-	preprocess: vitePreprocess(),
-
+	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 	kit: {
 		adapter: adapter()
 	}
 };
-
 export default config;
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./src/**/*.{html,js,ts,svelte}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
