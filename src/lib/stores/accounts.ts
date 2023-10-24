5ae993bedf5f9d7f06fb0c2e344952015b2b3ac1
bddvlpr <luna@bddvlpr.com>
2023-10-24 22:30:06 +0200

    merge: friends caching using derivation (#3)
    * feat: added friends derived
    
    * fix: active account reset
    
    * style: fix stores formatting

--- src/lib/stores/accounts.ts
+++ src/lib/stores/accounts.ts
@@ -1,4 +1,5 @@
 import { persisted } from 'svelte-persisted-store';
+import { get } from 'svelte/store';
 
 export type Account = {
 	identifier: string;
@@ -8,3 +9,7 @@ export type Account = {
 export const accounts = persisted<Account[]>('accounts', []);
 
 export const activeAccount = persisted<Account | null>('activeAccount', null);
+
+export const getActiveAccount = () => get(activeAccount);
+
+export const resetActiveAccount = () => activeAccount.set(null);
